6494f9721eb36f607654c575aee570806ed7ec53
pedroobando <pedroobando@hotmail.com>
2024-10-06 18:22:32 -0400

    04-teslo-shop fin fileupload

--- 04-teslo-shop/src/config/env.config.ts
+++ 04-teslo-shop/src/config/env.config.ts
@@ -2,4 +2,9 @@ export const EnvConfiguration = () => ({
   enviroment: process.env.NODE_ENV,
   hostapi: process.env.HOST_API,
   defaultLimit: +process.env.DEFAULT_LIMIT || 5,
+  s3_userName: process.env.S3_USERNAME,
+  s3_password: process.env.S3_PASSWORD,
+  s3_endpoint: process.env.S3_ENDPOINT,
+  s3_port: +process.env.S3_PORT || 9000,
+  s3_bucket: process.env.S3_BUCKET,
 });
